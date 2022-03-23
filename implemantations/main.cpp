618cf85ed69bfb03be4a0ef6ec0ba76574b755a2
Manu Ioan <ioanmanu@yahoo.com>
2022-03-23 12:14:39 +0200

    adjancency list

--- implemantations/main.cpp
+++ implemantations/main.cpp
@@ -52,17 +52,36 @@ int mda_file(int n, int a[100][100])
 
 void write(int n, int a[100][100])
 {
+    int s=0;
       for(i=0;i<n;i++)
 {
-
-
     for(j=0;j<n;j++)
+    {
+        if(a[i][j]==1)
+            s++;
+
     cout<<a[i][j]<<" ";
+    }
   cout<<endl;
 }
+cout<<endl<<"Avem un numar de "<<s/2<<" muchii.";
+}
+void adjacency_list(int n, int a[100][100])
+{
+    for(i=0;i<n;i++)
+    {   cout<<i<<"=";
+        for(j=0;j<n;j++)
+    {
+
+        if(a[i][j]==1)
+            cout<<j<<" ";
+
+    }
+    cout<<endl;
+    }
+
 
 }
-
 
 int main()
 {
@@ -76,7 +95,8 @@ fin>>n;
 //reading from file
   int a[100][100];
   mda_file(n, a);
-  write(n, a);
+ // write(n, a);
+ adjacency_list(n,a);
 
 
 
