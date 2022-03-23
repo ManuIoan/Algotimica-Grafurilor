f2cd3de532f9918333684b9a6429af9687ff770b
Manu Ioan <ioanmanu@yahoo.com>
2022-03-23 11:56:35 +0200

    Adjacency matrix

--- implemantations/main.cpp
+++ implemantations/main.cpp
@@ -2,8 +2,48 @@
 
 using namespace std;
 
+//matricea de adiacenta
+int i, j, ex;
+void mda(int n)
+{
+    int a[n+1][n+1];
+    for(i=0;i<n;i++)
+        for(j=i;j<n;j++)
+        if(i==j)
+        a[i][j]=0;
+    else
+    {
+        cout<<"Exista nod intre "<<i<<"si "<<j<<" ?0/1";
+        cin>>ex;
+        if(ex)
+           {
+           a[j][i]=1;
+           a[i][j]=1;
+           }
+           else
+            {
+           a[j][i]=0;
+           a[i][j]=0;
+           }
+
+
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++)
+    cout<<a[i][j]<<" ";
+    cout<<endl;
+    }
+
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
+  int n;
+cout<<"Cate noduri exista in graf?";
+cin>>n;
+    mda(n);
     return 0;
 }
